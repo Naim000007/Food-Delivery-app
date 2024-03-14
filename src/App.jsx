7966702dbc8a59ac76952e0fbf88431a09b66ddd
Naim <mdnaim01303202218@gmail.com>
2024-03-14 15:17:19 +0600

    project set up done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='w-screen h-screen flex items-center justify-center text-blue-600'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
