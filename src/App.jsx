a3afe3c48630175cc97c069f492f5456da8e7de8
Naim <mdnaim01303202218@gmail.com>
2024-03-15 10:25:31 +0600

    React Router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 
 import './App.css'
-import Header from './Components/Header/Header'
 
+import { Route, Routes } from 'react-router-dom'
+import { MainContainer, CreateContainer,Header } from './Connection';
 function App() {
-
 
   return (
     <>
-    <div className='w-screen h-auto flex-col'>
+    <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header/>
-      
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/' element={<MainContainer/>}/>
+          <Route path='/createItem' element={<CreateContainer/>}/>
+          <Route/>
+        </Routes>
+      </main>
     </div>
     </>
   )
---
+++ src/Connection.jsx
@@ -0,0 +1,3 @@
+export {default as Header}  from './Components/Header/Header'
+export {default as CreateContainer} from './Components/CreateContainer/CreateContainer'
+export {default as MainContainer} from './Components/MainContainer/MainContainer'
