06276e498f2440796664ad222034acc8ef8fd8cf
Naim <mdnaim01303202218@gmail.com>
2024-03-15 15:53:17 +0600

    header toggol done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/Img/logo.png';
 import avatar from '../../assets/Img/avatar.png';
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket, MdAdd , MdLogout } from "react-icons/md";
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -22,11 +22,12 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
     const [{ user }, dispatch] = useStateValue();
-
+    const [isMenu, setisMenu] = useState(false);
+ 
     // Function to handle login
     const login = async () => {
        if(!user){
-        try {
+        
             const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider);
             
             dispatch({
@@ -34,15 +35,15 @@ const Header = () => {
                 user: providerData[0]
             });
             localStorage.setItem('user', JSON.stringify(providerData[0]));
-        } catch (error) {
-            if (error.code === 'auth/cancelled-popup-request') {
-                console.log('User cancelled the login popup');
-            } else {
-                console.error('Error occurred during login:', error);
-            }
         }
+        else{
+            setisMenu(!isMenu);
+        }
+        
+        
+        
        }
-    };
+  
     
     return (
         <header className='fixed z-50 w-screen p-6 px-16'>
@@ -68,6 +69,20 @@ const Header = () => {
                     </div>
                     <div className='relative '>
                         <motion.img whileTap={{scale: 0.6}} src={user ? user.photoURL : avatar} alt="userimg" onClick={login} className='w-10   min-w-[40px] h-10 min-h-[40px] shadow-2xl cursor-pointer rounded-full' />
+                        {
+                            isMenu && (
+                                <div className='w-40 bg-gray-100 shadow-xl roundedllg flex flex-col absolute top-12 right-0 px-4 py-2'>
+                           {
+                            user && user.email === "mdnaim01910423877@gmail.com"  && (
+                                <Link to='/createItem'>
+                                <p  className=' py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd/></p>
+                                </Link>
+                            )
+                           }
+                           <p className=' flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Log Out  <MdLogout/></p>
+                        </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
