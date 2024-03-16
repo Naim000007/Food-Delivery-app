37d0ec4661d47a11eb8e8b7c2b5842f3947ddf6b
Naim <mdnaim01303202218@gmail.com>
2024-03-16 07:44:27 +0600

    header section done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,6 +11,7 @@ import { initializeApp } from 'firebase/app';
 import firebaseConfig from "../../../firebase.config";
 import { useStateValue } from '../../Context/StateProvider';
 import { actionType } from '../../Context/Reducer';
+import './Header.css'
 
 const Header = () => {
     // Initialize Firebase app
@@ -40,10 +41,16 @@ const Header = () => {
             setisMenu(!isMenu);
         }
 
-
-
     }
-
+    const logout =()=>{
+        setisMenu(false); 
+        localStorage.clear(); 
+        dispatch({
+            type: actionType.SET_USER,
+            user: null,
+        })
+    }
+    
 
     return (
         <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
@@ -83,7 +90,7 @@ const Header = () => {
                                             </Link>
                                         )
                                     }
-                                    <p className=' flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Log Out  <MdLogout /></p>
+                                    <p className=' flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'  onClick={logout}>Log Out  <MdLogout /></p>
                                 </div>
                             )
                         }
@@ -117,7 +124,7 @@ const Header = () => {
                                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100">About Us</li>
                                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100">Service</li>
                                 </ul>
-                                <p className=' flex items-center gap-2 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base px-4 '>Log Out  <MdLogout /></p>
+                                <p className='mb-2 p-2 rounded-md shadow-md flex items-center justify-center gap-2  cursor-pointer hover:bg-slate-400 transition-all duration-100 ease-in-out text-textColor text-base  bg-gray-200' onClick={logout}>Log Out  <MdLogout /></p>
                             </div>
                         )
                     }
